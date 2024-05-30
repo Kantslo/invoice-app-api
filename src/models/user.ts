a2242cc4856efd41f797db0029fe49819abc9be1
kantslo <gqancliani@gmail.com>
2024-05-30 23:44:42 +0400

    feat: created user model using mongoose

---
+++ src/models/user.ts
@@ -0,0 +1,26 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  invoices: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Invoice",
+    },
+  ],
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
