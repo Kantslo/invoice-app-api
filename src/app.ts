a06effdd165f6d9907fe7302d815849974b010a3
kantslo <gqancliani@gmail.com>
2024-06-01 03:41:30 +0400

    feat: added express error middleware in main file

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,17 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 
+import authRoutes from "./routes/auth.js";
+import errorHandler from "./controllers/error-handler.js";
+
 const app = express();
 
 app.use(bodyParser.json());
 
 app.use(cors());
+
+app.use("/", authRoutes);
+
+app.use(errorHandler);
 
 app.listen(3000);
