e2b331c698ad63713bb9092377768f678354bb6f
kantslo <gqancliani@gmail.com>
2024-06-02 07:40:17 +0400

    feat: added signup route to authentication router

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,6 +1,10 @@
 import express from "express";
 
+import { signup } from "src/controllers/index.js";
+
 const router = express.Router();
+
+router.post("/signup", signup);
 
 router.get("/login");
 
